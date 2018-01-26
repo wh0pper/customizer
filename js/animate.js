a16cdad5de907092578941e505043d65e0370fe6
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-01-26 13:03:46 -0800

    create basic table objects

--- js/animate.js
+++ js/animate.js
@@ -1,6 +1,7 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
+
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
@@ -16,13 +17,20 @@ scene.add( cube );
 
 //table top geometry
 
-var topGeometry = new THREE.BoxGeometry( 20, 2, 10 );
-var topMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var topGeometry = new THREE.BoxGeometry( 20, 1, 10 );
+var topMaterial = new THREE.MeshBasicMaterial( { color: 0xd2b48c } );
 var tableTop = new THREE.Mesh( topGeometry, topMaterial );
 scene.add( tableTop );
 
 //table legs geom
-
+var legOneGeometry = new THREE.BoxGeometry( 1, 10, 1)
+var legTwoGeometry = new THREE.BoxGeometry( 1, 10, 1)
+var legThreeGeometry = new THREE.BoxGeometry( 1, 10, 1)
+var legFourGeometry = new THREE.BoxGeometry( 1, 10, 1)
+var legOneMaterial = new THREE.MeshBasicMaterial( { color: 0xd2b48c } );
+var legTwoMaterial = new THREE.MeshBasicMaterial( { color: 0xd2b48c } );
+var legThreeMaterial = new THREE.MeshBasicMaterial( { color: 0xd2b48c } );
+var legFourMaterial = new THREE.MeshBasicMaterial( { color: 0xd2b48c } );
 // //line geometry and material
 // var lineMat = new THREE.LineBasicMaterial({ color: 0x0000ff });
 //
@@ -34,8 +42,6 @@ scene.add( tableTop );
 // //combine geo and mat into line object
 // var line = new THREE.Line(lineGeo, lineMat);
 // scene.add(line);
-
-
 
 
 
